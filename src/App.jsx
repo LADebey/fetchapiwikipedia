d3a8137f484ad1098cf31114f592de1cecbcf929
LADebey <louisadriendebey@gmail.com>
2023-12-14 00:00:19 +0100

    Update visual

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,48 @@
 import { useState } from "react";
-
 import "./App.css";
 
 const Search = () => {
   const [article, setArticle] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
   async function getArticle() {
     let response = await fetch(
-      ` https://fr.wikipedia.org/w/api.php?origin=*&action=opensearch&search=${searchValue}&format=json`
+      `https://fr.wikipedia.org/w/api.php?origin=*&action=opensearch&search=${searchValue}&format=json`
     );
     let data = await response.json();
     setArticle(data);
     console.log(data);
   }
-  const [searchValue, setSearchValue] = useState("");
+
   const handleInputChange = (e) => {
     setSearchValue(e.target.value);
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        name="searchValue"
-        onChange={handleInputChange}
-        placeholder="Search"
-      ></input>
-      <button onClick={getArticle}>Search</button>
-      {article[1]?.map((element, index) => {
-        console.log(article[3]);
-        return (
-          <div key={index}>
-            <a href={article[3][0]} target="_blank">
-              {element}
-            </a>
-          </div>
-        );
-      })}
+    <div className="container">
+      <div className="searchArea">
+        <input
+          type="text"
+          name="searchValue"
+          onChange={handleInputChange}
+          placeholder="Rechercher"
+        />
+        <button onClick={getArticle}>Rechercher</button>
+      </div>
+      <div className="resultsArea">
+        <div className="resultsList">
+          {article[1]?.map((element, index) => {
+            console.log(article[3]);
+            return (
+              <div key={index}>
+                <a href={article[3][0]} target="_blank">
+                  {element}
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
